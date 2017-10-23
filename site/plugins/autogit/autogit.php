9c3ede7d703863329d28679b73db2fd3c52d0caf
Steve Coates <stevecoates@Steves-MacBook-Pro.local>
2017-10-23 20:42:12 +0100

    First commit

---
+++ site/plugins/autogit/autogit.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Kirby Auto Git Plugin
+ *
+ * @version   0.5.0
+ * @author    Pedro Borges <oi@pedroborg.es>
+ * @copyright Pedro Borges <oi@pedroborg.es>
+ * @link      https://github.com/pedroborges/kirby-autogit
+ * @license   MIT
+ */
+
+// Load Auto Git class and dependencies
+require_once(__DIR__.DS.'vendor'.DS.'autoload.php');
+require_once(__DIR__.DS.'lib'.DS.'autogit.php');
+
+// Helper function that returns an Autogit\Autogit instance
+function autogit() {
+    return Autogit\Autogit::instance();
+}
+
+// Load routes
+if (c::get('autogit.webhook.secret', false)) {
+    require_once(__DIR__.DS.'lib'.DS.'routes.php');
+}
+
+// Only load hooks, routes and widgets when
+// the content directory is a Git repo
+if (function_exists('panel') && autogit()->isRepo()) {
+    // Load hooks
+    require_once(__DIR__.DS.'lib'.DS.'hooks.php');
+
+    // Load widgets
+    if (c::get('autogit.widget', true)) {
+        kirby()->set('widget', 'autogit', __DIR__.DS.'widgets'.DS.'autogit');
+    }
+}
