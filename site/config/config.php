30049f8df4a9c86f83e514bd0842ed7e3092960c
Steve Coates <stevecoates@Steves-MacBook-Pro.local>
2017-10-23 21:49:09 +0100

    f

--- site/config/config.php
+++ site/config/config.php
@@ -29,5 +29,5 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
 
-//c::set('autogit.branch', 'content_changes');
+c::set('autogit.branch', 'content_changes');
 c::set('autogit.webhook.secret', 'october');
